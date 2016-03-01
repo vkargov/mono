cef05f6c6d1b6dd90d4a3b71586809203103cd3e
Vladimir Kargov <kargov@gmail.com>
2016-03-01 15:36:40 -0800

    Update linediff.

--- tools/linediff.py
+++ tools/linediff.py
@@ -1,3 +1,5 @@
+#!/usr/bin/sh
+
 import sys
 import re
 
@@ -5,12 +7,15 @@ left_file = open(sys.argv[1], 'r')
 right_file = open(sys.argv[2], 'r')
 lineno = 0
 
+nopref_regexp = re.compile('\[[^]]*\]\s*')
+nohex_regexp = re.compile('0x[0-9A-Fa-f]*|\(nil\)')
+
 while True:
     lines = {}
     lines[0] = left_file.readline()
     lines[1] = right_file.readline()
-    nopref_regexp = re.compile('\[[^]]*\]\s*')
-    nohex_regexp = re.compile('0x[0-9A-Fa-f]*')
+    if lines[0] == '' or lines[1] == '':
+        sys.exit(1)
     for i in range(2):
         lines[i] = nopref_regexp.sub('', lines[i])
         lines[i] = nohex_regexp.sub('', lines[i])
@@ -18,8 +23,6 @@ while True:
         print('Line {0} differs!'.format(lineno))
         print('< {0}'.format(lines[0]), end='')
         print('> {0}'.format(lines[1]), end='')
-        if lines[0] == '' or lines[0] == '':
-            sys.exit(1)
     lineno += 1
 
 sys.exit(0)
